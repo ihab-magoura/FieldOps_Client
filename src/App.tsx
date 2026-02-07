99b3d66e03539e1f3bf19c9aa4de73283efbe763
ihab-magoura <ihab.magoura@gmail.com>
2026-02-07 16:18:58 +0100

    Initial commit: Project setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './styles/index.css'
+import { Button } from "@/components/ui/button"
+function App() {
+
+  return (
+    <>
+         <div className="w-20 h-50 bg-indigo-400">
+        <Button>hello</Button>
+         </div>
+    </>
+  )
+}
+
+export default App
